68df30f7e408b59a982f7151fa4d5277f57ff61c
Hezron Nyamboga <hezzyn1@gmail.com>
2024-01-11 20:26:52 +0300

    allow cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 
 const sendMailRoute = require('./routes/sendMailRoute')
@@ -8,6 +9,12 @@ const notFoundMiddleware = require('./middleware/notFound');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+const corsOptions = {
+  origin: 'http://localhost:3001/index.html', 
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 // routes
 app.use('/api', sendMailRoute)
